00f4c88a029aacaacbfc32e3f707431683e944c2
Mã Đằng <mateng@npackr.com>
2022-08-02 15:43:14 +0700

    Add name to sidebar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,20 +1,8 @@
 <template>
   <v-app dark>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-    >
+    <v-navigation-drawer v-model="drawer" :mini-variant="miniVariant" :clipped="clipped" fixed app>
       <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
+        <v-list-item v-for="(item, i) in items" :key="i" :to="item.to" router exact>
           <v-list-item-action>
             <v-icon>{{ item.icon }}</v-icon>
           </v-list-item-action>
@@ -37,8 +25,8 @@
       </v-btn>
       <v-toolbar-title v-text="title" />
       <v-spacer />
-      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
-        <v-icon>mdi-account</v-icon>
+      <v-btn @click.stop="rightDrawer = !rightDrawer">
+      NGUYỄN ANH KIỆT
       </v-btn>
     </v-app-bar>
     <v-main>
@@ -48,6 +36,28 @@
     </v-main>
     <v-navigation-drawer v-model="rightDrawer" :right="right" temporary fixed>
       <v-list>
+        <v-list-item>
+          <v-list-item-avatar>
+            <v-img src="https://cdn.vuetifyjs.com/images/john.png"></v-img>
+          </v-list-item-avatar>
+        </v-list-item>
+
+        <v-list-item link>
+          <v-list-item-content>
+            <v-list-item-title class="text-h6">
+              NGUYỄN ANH KIỆT
+            </v-list-item-title>
+            <v-list-item-subtitle>MSSV: 1851.062.658</v-list-item-subtitle>
+          </v-list-item-content>
+
+          <v-list-item-action>
+            <v-icon>mdi-menu-down</v-icon>
+          </v-list-item-action>
+        </v-list-item>
+      </v-list>
+      <v-divider></v-divider>
+      <v-list>
+
         <v-list-item @click.native="right = !right">
           <v-list-item-action>
             <v-icon> mdi-repeat </v-icon>
